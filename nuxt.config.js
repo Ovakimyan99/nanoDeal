17198c6c6e5b6dd8528ee5141d6f70c1a2a4f05a
tigran <tigr.kman@gmail.com>
2021-09-05 23:51:06 +0300

    dist

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,6 +41,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    publicPath: '/nanoDeal/'
+    publicPath: '/nanoDeal/',
+    analyze: {
+      analyzerMode: 'static'
+    }
   }
 }
