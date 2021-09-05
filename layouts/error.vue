25ab610fce256e6957d838fbec210696b92a81c8
tigran <tigr.kman@gmail.com>
2021-09-05 23:31:17 +0300

    добавил error layout

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <span>что - то пошло н етак, переместитесь на другую страницу, пожалуйста</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
